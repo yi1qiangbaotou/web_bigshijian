82707273feee9be5214a10efe4baf04856ecfc51
王成 <673501986@qq.com>
2020-10-15 15:03:44 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,9 +20,9 @@ function getUserInfo() {
     $.ajax({
       method: 'GET',
       url: '/my/userinfo',
-      headers: {
-        Authorization: localStorage.getItem('token') || ''
-      },
+    //   headers: {
+    //     Authorization: localStorage.getItem('token') || ''
+    //   },
       success: function(res) {
         if (res.status !== 0) {
           return layui.layer.msg('获取用户信息失败！')
@@ -64,4 +64,5 @@ function renderAvatar(user) {
         .html(first)
         .show()
     }
-  }
+}
+  
